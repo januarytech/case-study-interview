46c9f76c8f8ef6b9b716012ea8e1ecc4e2a64937
linyahu <lh62594@gmail.com>
2024-03-22 18:15:41 -0400

    adding more methods

--- server/controllers/card_holder_controller.py
+++ server/controllers/card_holder_controller.py
@@ -9,5 +9,13 @@ class CardHolderController(BaseController):
     def get_card_holders(self, search_params):
         return self.get_all(**search_params)
 
+    def get_card_holders_by_name(self, name):
+        card_holders = CardHolder.query.filter(CardHolder.name.ilike(f'%{name}%')).all()
+        return jsonify([card_holder.to_dict() for card_holder in card_holders])
+
+    def get_card_holders_by_card_issuance_date(self, issuance_date):
+        card_holders = CardHolder.query.join(CreditCard).filter(CreditCard.issuance_date == issuance_date).all()
+        return jsonify([card_holder.to_dict() for card_holder in card_holders])
+
 
 card_holder_controller = CardHolderController()
--- server/controllers/credit_card_controller.py
+++ server/controllers/credit_card_controller.py
@@ -10,5 +10,18 @@ class CreditCardController(BaseController):
     def get_credit_cards_by_card_holder(self, card_holder_id):
         return self.get_all(card_holder_id=card_holder_id)
 
+    def get_credit_card_by_number(self, card_number):
+        credit_card = CreditCard.query.filter_by(card_number=card_number).first()
+        return jsonify(credit_card.to_dict() if credit_card else {})
+
+    def calculate_next_payment_date(self, card_number):
+        credit_card = CreditCard.query.filter_by(card_number=card_number).first()
+        if credit_card:
+            # Assuming next payment date is 30 days after the last payment date
+            next_payment_date = credit_card.next_payment_date + timedelta(days=30)
+            return jsonify({'next_payment_date': next_payment_date.strftime('%Y-%m-%d')})
+        else:
+            return jsonify({'error': 'Credit card not found'})
+
 
 credit_card_controller = CreditCardController()
